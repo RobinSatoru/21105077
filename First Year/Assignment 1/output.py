94e58f73cc9de05d1a4b8c84b059081d3c2f4dd7
Robin Singh <96612340+RobinSatoru@users.noreply.github.com>
2022-01-14 21:50:31 +0530

    Add files via upload

---
+++ First Year/Assignment 1/output.py
@@ -0,0 +1,37 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Sandeep\Downloads\Books\pec\First Year\Assignment 1\ece_21105077_RobinSingh.py
+          1st PROGRAM
+
+Enter first number: 12
+Enter second number: 24
+Enter third number: 56
+The average of the three numbers is =  30.666666666666668
+
+          2nd PROGRAM
+
+Enter your gross income: 30002.98
+Enter number of dependents: 2
+Your taxable income is: $ 14002.98
+Your net income tax: $ 2800.6
+
+          3rd PROGRAM
+
+student = [SID, Name, Gender, Branch, CGPA]
+output =  [21105077, 'Robin Singh', 'M', 'ECE', 9.99]
+
+          4th PROGRAM
+
+Enter marks of Student  1 : 56
+Enter marks of Student  2 : 98
+Enter marks of Student  3 : 38
+Enter marks of Student  4 : 87
+Enter marks of Student  5 : 88
+Sorted marks:  [38.0, 56.0, 87.0, 88.0, 98.0]
+
+          5th PROGRAM
+
+Original list:  ['red', 'green', 'white', 'black', 'pink', 'yellow']
+After removing 4th element:  ['red', 'green', 'white', 'pink', 'yellow']
+After replacing black and pink with purple:  ['red', 'green', 'white', 'purple', 'yellow']
