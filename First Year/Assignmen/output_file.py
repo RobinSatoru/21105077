521e2e1200679a251aefdfbf3a0224f4725c66ff
Robin Singh <96612340+RobinSatoru@users.noreply.github.com>
2022-01-23 18:44:00 +0530

    Add files via upload

---
+++ First Year/Assignmen/output_file.py
@@ -0,0 +1,46 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:/Users/Sandeep/Downloads/Books/pec/First Year/source_code.py ===
+         QUESTION - 1
+
+Input string =  Python is a case sensitive language
+a. Length of the input string is 35 characters
+b. Reversed string(using extended slice) = egaugnal evitisnes esac a si nohtyP
+c. Sliced String = a case sensitive
+d. New string obtained after replacement = Python is object oriented language
+e. The substring 'a' was found at index = 10, 13, 28, 32
+f. New string with spaces removed = Pythonisacasesensitivelanguage
+
+         QUESTION - 2
+
+Hey! Robin Singh here!
+My SID is 21105077
+I am from ECE department and my CGPA is 10.000000
+
+         QUESTION - 3
+
+a. a&b = 8
+b. a|b = 58
+c. a^b = 50
+d. a << 2 = 224 b << 2 = 40
+e. a >> 2 = 14 b >> 4 = 0
+
+         QUESTION - 4
+
+Enter number 1: 1
+Enter number 2: 8
+Enter number 3: 2.3
+The greatest number = 8.0
+
+         QUESTION - 5
+
+Enter your string: si
+No
+
+         QUESTION - 6
+
+Enter side 1: 2
+Enter side 2: 5
+Enter side 3: 4
+Yes
