91d1c88169d0219cb18f897b4c09012f31637c7d
Robin Singh <96612340+RobinSatoru@users.noreply.github.com>
2022-02-14 23:55:09 +0530

    Create output.py

---
+++ First Year/Assignment 3/output.py
@@ -0,0 +1,89 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:\Users\Sandeep\AppData\Local\Programs\Python\Python310\pyass3.py =
+
+       QUESTION - 1
+
+Enter the string: I I am the best of best
+The occurence(s) of:
+i : 2
+am : 1
+the : 1
+best : 2
+of : 1
+
+       QUESTION - 2
+
+Please enter numerical values only.
+
+Day -29
+Month -02
+Year -2016
+The next Date is 01/03/2016
+
+       QUESTION - 3
+
+How many numbers do you want to add? 
+3
+Enter number 1: 19
+Enter number 2: 8
+Enter number 3: 34
+Result: [(19, 361), (8, 64), (34, 1156)]
+
+       QUESTION - 4
+
+Enter grade of student
+8
+Your Grade is B+ and Very Good.
+
+       QUESTION - 5
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+       QUESTION - 6
+
+Please enter unique SID and correct Name everytime.
+Enter SID of Student - 21105077
+Enter Name of Student - Robin Singh
+Do you want add more students? Enter Y or N - y
+
+Enter SID of Student - 21105005
+Enter Name of Student - Soumil Arora
+Do you want add more students? Enter Y or N - y
+
+Enter SID of Student - 21105079
+Enter Name of Student - Vanshul Goyal
+Do you want add more students? Enter Y or N - n
+
+a) The Students' details are as follows - 
+SID - 21105077 Name - Robin Singh
+SID - 21105005 Name - Soumil Arora
+SID - 21105079 Name - Vanshul Goyal
+
+b) Dictionary sorted by student names = {21105077: 'Robin Singh', 21105005: 'Soumil Arora', 21105079: 'Vanshul Goyal'}
+
+c) Dictionary sorted by SID's = {21105005: 'Soumil Arora', 21105077: 'Robin Singh', 21105079: 'Vanshul Goyal'}
+
+d) Student details' search
+Enter Student's SID - 21105079
+Student's name is Vanshul Goyal
+
+       QUESTION - 7
+
+How many terms do you want to print? 19
+0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 
+Average of first 19 terms = 356.0
+
+       QUESTION - 8
+
+a) {1, 3, 5, 6, 8}
+b) {17, 3, 6, 8, 9, 13}
+c) {1, 2, 4, 5}
+d) {6, 7, 8, 9, 10}
+e) {10, 7}
