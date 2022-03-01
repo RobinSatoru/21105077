bbc4fca71978e8b798eec3c2e04f5142e2d5cd5f
Robin Singh <96612340+RobinSatoru@users.noreply.github.com>
2022-03-01 23:24:38 +0530

    Create output.py

---
+++ First Year/Assignment 4/output.py
@@ -0,0 +1,88 @@
+           QUESTION - 1
+
+move 1 disk from stack 1 to stack 2
+move 1 disk from stack 1 to stack 3
+move 1 disk from stack 2 to stack 3
+move 1 disk from stack 1 to stack 2
+move 1 disk from stack 3 to stack 1
+move 1 disk from stack 3 to stack 2
+move 1 disk from stack 1 to stack 2
+
+           QUESTION - 2
+
+Enter number of lines - 10
+Using Iteration -
+1 
+1 1 
+1 2 1 
+1 3 3 1 
+1 4 6 4 1 
+1 5 10 10 5 1 
+1 6 15 20 15 6 1 
+1 7 21 35 35 21 7 1 
+1 8 28 56 70 56 28 8 1 
+1 9 36 84 126 126 84 36 9 1 
+
+Using Recursion -
+1 
+1 1 
+1 2 1 
+1 3 3 1 
+1 4 6 4 1 
+1 5 10 10 5 1 
+1 6 15 20 15 6 1 
+1 7 21 35 35 21 7 1 
+1 8 28 56 70 56 28 8 1 
+1 9 36 84 126 126 84 36 9 1 
+
+           QUESTION - 3
+
+Enter first number - 28
+Enter second number - 8
+Quotient: 3    Remainder: 4
+
+a)callable() function was used to check if the function(divmod()) was callable.
+Answer is True
+
+b)All values are not zero
+
+c)After adding values - (3, 4, 4, 5, 6)
+After filtration - (3, 4, 4)
+
+d)After converting into set datatype - {3, 4}
+
+e)Conversion into immutable set - frozenset({3, 4})
+
+f)Maximum value of set = 4 
+It's hash value = -8296090686598762464
+
+           QUESTION - 4
+
+Enter name of student: Robin Singh
+Enter SID of student: 21105077
+
+The name of the student is Robin Singh and his/her roll number is 21105077
+
+Object destroyed
+
+           QUESTION - 5
+
+Employee Details:
+Name - Mehak     Salary - 40000
+Name - Ashok     Salary - 50000
+Name - Viren     Salary - 60000
+
+Updated Details:
+Name - Mehak     Salary - 70000
+Name - Ashok     Salary - 50000
+Name - Viren     Salary - 60000
+
+Updated Details:
+Name - Mehak     Salary - 70000
+Name - Ashok     Salary - 50000
+
+           QUESTION - 6
+
+Enter word uttered by George - fried
+Enter word made by Barbie - fired
+Their friendship is real.
